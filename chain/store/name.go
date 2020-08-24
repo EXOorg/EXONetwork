47eb0a71271fe3af36582d9f1d5fc504d0f0dde7
Goodale <TomGoodale@hotmail.com>
2020-08-24 13:39:14 +0800

    1.add name transfer function 2.make FinalizeNames compatible with legacy type
    Signed-off-by: Goodale <TomGoodale@hotmail.com>

--- chain/store/name.go
+++ chain/store/name.go
@@ -8,6 +8,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/nknorg/nkn/util/config"
+
 	"github.com/nknorg/nkn/common/serialization"
 )
 
@@ -120,6 +122,17 @@ func (sdb *StateDB) registerName(name string, registrant []byte, expiresAt uint3
 	return sdb.cleanupNamesAtHeight(expiresAt, nameId)
 }
 
+func (sdb *StateDB) transferName(name string, to []byte) error {
+	nameId := getNameId(name)
+
+	ni, err := sdb.getNameInfo(nameId)
+	if err != nil {
+		return err
+	}
+	ni.registrant = to
+	return nil
+}
+
 func (sdb *StateDB) updateNameInfo(nameId string, ni *nameInfo) error {
 	nibytes, err := ni.Bytes()
 	if err != nil {
@@ -139,6 +152,11 @@ func (sdb *StateDB) deleteNameInfo(nameId string) error {
 }
 
 func (sdb *StateDB) FinalizeNames(commit bool) {
+	_, height, _ := sdb.cs.getCurrentBlockHashFromDB()
+	if config.LegacyNameService.GetValueAtHeight(height) {
+		sdb.FinalizeNames_legacy(commit)
+		return
+	}
 	sdb.names.Range(func(key, value interface{}) bool {
 		if nameId, ok := key.(string); ok {
 			if info, ok := value.(*nameInfo); ok && !info.Empty() {
