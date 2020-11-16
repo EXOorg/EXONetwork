c8f72837197aaca4695b608f304d6e97d38a543c
Bernie <BernieGao@hotmail.com>
2020-11-16 08:52:39 +0800

    1.Will not cache block with invalid signature 2.Add neighbor sent block with invalid signature to blacklist
    Signed-off-by: Bernie <BernieGao@hotmail.com>

--- consensus/proposal.go
+++ consensus/proposal.go
@@ -272,6 +272,10 @@ func (consensus *Consensus) receiveProposalHash(neighborID string, height uint32
 		return nil
 	}
 
+	if _, ok := consensus.neighborBlacklist.Load(neighborID); ok {
+		return fmt.Errorf("ignore block hash %s from blacklist neighbor %s", blockHash.ToHexString(), neighborID)
+	}
+
 	expectedHeight := consensus.GetExpectedHeight()
 	if height != expectedHeight {
 		return fmt.Errorf("Receive invalid block hash height %d instead of %d", height, expectedHeight)
@@ -348,6 +352,13 @@ func (consensus *Consensus) requestProposal(neighbor *node.RemoteNode, blockHash
 			consensus.proposals.Set(blockHash.ToArray(), b)
 			return nil, fmt.Errorf("Proposal fails to pass signer check: %v", err)
 		}
+	}
+
+	if err = chain.SignatureCheck(b.Header); err != nil {
+		err = fmt.Errorf("Proposal fails to pass signature check: %v", err)
+		consensus.neighborBlacklist.Store(neighbor.GetID(), err)
+		log.Infof("Add neighbor %s to blacklist because: %v", neighbor.GetID(), err)
+		return nil, err
 	}
 
 	var txnsRoot common.Uint256
